9120ee5f32387c5143efb83fd7a96f182bc6ca98
VladAvdeev <vladislav.avdeev55@gmail.com>
2021-04-21 15:16:57 +0300

    water

--- WaterVolumeAfterRain/WaterVolumeAfterRain/Program.cs
+++ WaterVolumeAfterRain/WaterVolumeAfterRain/Program.cs
@@ -6,40 +6,11 @@ namespace WaterVolumeAfterRain
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] { 2, 5, 1, 2, 3, 4, 7, 7, 6 };
-            Console.WriteLine(GetVolume(array));
+            int[] waterWall = new int[] { 2, 5, 1, 2, 3, 4, 7, 7, 6 };
+            Volume volume = new Volume();
+            Console.WriteLine(volume.GetVolume(waterWall));
         }
-        public static int GetVolume(int[] nums)
-        {
-            int leftMax = 0;
-            int rightMax = 0;
-            int left = 0;
-            int right = nums.Length - 1;
-            int volume = 0;
-
-            while( left < right)
-            {
-                if(nums[left] > leftMax)
-                {
-                    leftMax = nums[left];
-                }
-                if(nums[right] > rightMax)
-                {
-                    rightMax = nums[right];
-                }
-                if(leftMax >= rightMax)
-                {
-                    volume += rightMax - nums[right];
-                    right--;
-                }
-                else
-                {
-                    volume += leftMax - nums[left];
-                    left++;
-                }
-
-            }
-            return volume;
-        }
+        
     }
 }
+
---
+++ WaterVolumeAfterRain/WaterVolumeAfterRain/Volume.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WaterVolumeAfterRain
+{
+    class Volume
+    {
+        public int GetVolume(int[] waterWall)
+        {
+
+            int leftMax = 0; // наибольшее значение слева
+            int rightMax = 0; // наибольшее значение справа
+            int left = 0; // левый указатель
+            int right = waterWall.Length - 1; //правый указатель
+            int volume = 0;
+
+            while (left < right)
+            {
+                if (waterWall[left] > leftMax)
+                {
+                    leftMax = waterWall[left]; // сдвиг левого указателя
+                }
+                if (waterWall[right] > rightMax)
+                {
+                    rightMax = waterWall[right]; // сдвиг правого указателя
+                }
+                if (leftMax >= rightMax)
+                {
+                    volume += rightMax - waterWall[right];
+                    right--;
+                }
+                else
+                {
+                    volume += leftMax - waterWall[left];
+                    left++;
+                }
+
+            }
+            return volume;
+        }
+    }
+}
